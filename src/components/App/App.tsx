f4b372042043c23001f6470fcb1ecdf297f4ec36
sruthi sripathi <sruthisripathi7@gmail.com>
2020-09-23 17:46:22 +0530

    Initial commit: Boilerplate

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'; 
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
